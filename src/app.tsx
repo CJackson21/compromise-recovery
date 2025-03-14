ccc22fceae527eed5f326d1d33ccce012e3cb1a7
Caleb Jackson <calebjackson2002@gmail.com>
2025-03-13 20:48:16 -0700

    chore: added generic pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { Button, Typography, Paper } from "@mui/material";
-import Layout from "./components/Layout"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
+// Placeholder Components
+// The pages are in /src/components and need to be stubbed out
+const Home = () => <h1 className="text-3xl font-bold">Home Page</h1>;
+const Social = () => <h1 className="text-3xl font-bold">Social Page</h1>;
+const Family = () => <h1 className="text-3xl font-bold">Family Page</h1>;
+const Devices = () => <h1 className="text-3xl font-bold">Devices Page</h1>;
+const Physical = () => <h1 className="text-3xl font-bold">Physical Page</h1>;
+const NotFound = () => <h1 className="text-3xl font-bold">404 - Not Found</h1>;
 
 export default function App() {
-  const [count, setCount] = React.useState(0);
-
   return (
-    <Layout>
-      <div className="mt-8">
-       
-      </div>
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/social" element={<Social />} />
+          <Route path="/family" element={<Family />} />
+          <Route path="/devices" element={<Devices />} />
+          <Route path="/physical" element={<Physical />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
