95048329a3b7e7489fd8272f01b2ee14d21963ad
Caleb Jackson <calebjackson2002@gmail.com>
2025-03-13 20:29:22 -0700

    fix: import issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./App.css"; // <--- Important: Tailwind & global styles
+import App from "./app";
+import "./App.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
