7a2c0768184a4356756fac49508eb05d763e61aa
Caleb Jackson <calebjackson2002@gmail.com>
2025-03-14 23:41:42 -0700

    fix: lint fix and fix script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,5 +2,9 @@
 if npx eslint 'src/**/*.{js,jsx,ts,tsx}'; then
     echo "✅ No issues found!"
 else
-    echo "❌ Linting errors found!"
+    echo "❌ Linting errors found! Fixing..."
+    npx eslint --fix 'src/**/*.{js,jsx,ts,tsx}'
+    if npx eslint 'src/**/*.{js,jsx,ts,tsx}'; then
+        echo "✅ Fixed!"
+    fi
 fi
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,58 +1,58 @@
 import { createTheme } from '@mui/material/styles';
 
 const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-    background: {
-      default: '#e0e0e0', 
-      paper: '#ffffff',   
-    },
-    primary: {
-      main: '#646cff',
-    },
-    text: {
-      primary: '#213547',
-    },
-  },
-  typography: {
-    h4: {
-      fontSize: '2.5rem',
-      fontWeight: 700,
-      '@media (max-width:600px)': {
-        fontSize: '2rem',
-      },
-    },
-    h5: {
-      fontSize: '2rem',
-      fontWeight: 600,
-      '@media (max-width:600px)': {
-        fontSize: '1.5rem',
-      },
-    },
-    h6: {
-      fontSize: '1.5rem',
-      fontWeight: 500,
-      '@media (max-width:600px)': {
-        fontSize: '1.25rem',
-      },
-    },
-    body1: {
-      fontSize: '1.15rem',
-      lineHeight: 1.7,
-      '@media (max-width:600px)': {
-        fontSize: '1rem',
-      },
-    },
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          backgroundColor: '#edf3fc',
-        },
-      },
-    },
-  },
+	palette: {
+		mode: 'light',
+		background: {
+			default: '#e0e0e0', 
+			paper: '#ffffff',   
+		},
+		primary: {
+			main: '#646cff',
+		},
+		text: {
+			primary: '#213547',
+		},
+	},
+	typography: {
+		h4: {
+			fontSize: '2.5rem',
+			fontWeight: 700,
+			'@media (max-width:600px)': {
+				fontSize: '2rem',
+			},
+		},
+		h5: {
+			fontSize: '2rem',
+			fontWeight: 600,
+			'@media (max-width:600px)': {
+				fontSize: '1.5rem',
+			},
+		},
+		h6: {
+			fontSize: '1.5rem',
+			fontWeight: 500,
+			'@media (max-width:600px)': {
+				fontSize: '1.25rem',
+			},
+		},
+		body1: {
+			fontSize: '1.15rem',
+			lineHeight: 1.7,
+			'@media (max-width:600px)': {
+				fontSize: '1rem',
+			},
+		},
+	},
+	components: {
+		MuiCssBaseline: {
+			styleOverrides: {
+				body: {
+					backgroundColor: '#edf3fc',
+				},
+			},
+		},
+	},
 });
 
 export default lightTheme;
