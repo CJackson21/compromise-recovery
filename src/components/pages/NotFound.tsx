91c668b1711fea6a11fe31d7cf839d2ca8411c4b
Caleb Jackson <calebjackson2002@gmail.com>
2025-03-28 19:21:30 -0700

    feat: added page in case of 404 not found

---
+++ src/components/pages/NotFound.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Grid from '@mui/material/Grid2';
+import Typography from '@mui/material/Typography';
+
+function NotFound() {
+    return (
+        <Grid spacing={2} container direction="column" className="w-full max-h-screen">
+            {/* Header / message */}
+            <Grid size={{ xs: 'auto' }}>
+                <Typography variant="h4" align="center" fontWeight="bold">
+                    Sorry! The page you are looking for could not be found.
+                </Typography>
+            </Grid>
+
+            {/* Main / dancing cat area */}
+            <Grid size={12} className="flex items-center justify-center overflow-hidden mt-5">
+                <img
+                    src="/img/dancing_cat.gif"
+                    alt="Dancing Cat"
+                    className="min-h-[60vh] max-w-[60vw]"
+                />
+            </Grid>
+        </Grid>
+    );
+}
+
+export default NotFound;
